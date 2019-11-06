311b50f663070b2c707b7b0d7f77a8bf5c52434d
nghi dep trai <luubieunghi@gmail.com>
2019-11-06 10:27:39 +0700

    Add onClickListener for RecyclerView

--- app/src/main/java/luubieunghi/lbn/booklib/adapter/BookRecyclerViewAdapter.java
+++ app/src/main/java/luubieunghi/lbn/booklib/adapter/BookRecyclerViewAdapter.java
@@ -1,17 +1,21 @@
 package luubieunghi.lbn.booklib.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+import luubieunghi.lbn.booklib.Activity.MainActivity;
+import luubieunghi.lbn.booklib.Activity.ReadBookActivity;
 import luubieunghi.lbn.booklib.R;
 import luubieunghi.lbn.booklib.model.Book;
 
@@ -28,8 +32,19 @@ public class BookRecyclerViewAdapter extends RecyclerView.Adapter<BookRecyclerVi
     @Override
     public BookRecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        context = parent.getContext();
         View view = inflater.inflate(R.layout.item_book, parent, false);
-        return new BookRecyclerViewHolder(view);
+        final BookRecyclerViewHolder holder = new BookRecyclerViewHolder(view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer position = holder.getAdapterPosition();
+                Toast.makeText(context, "Clicked book number " + position.toString(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context, ReadBookActivity.class);
+                context.startActivity(intent);
+            }
+        });
+        return holder;
     }
 
     @Override
