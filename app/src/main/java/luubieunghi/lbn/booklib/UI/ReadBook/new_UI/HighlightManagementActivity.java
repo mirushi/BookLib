714413fef494e0e68388b368e2c8aae87d314aa0
Dong Nguyen Van <33110542+dongnvsince1999@users.noreply.github.com>
2019-12-27 17:28:50 +0700

    Add highlight manager fragment

--- app/src/main/java/luubieunghi/lbn/booklib/UI/ReadBook/new_UI/HighlightManagementActivity.java
+++ app/src/main/java/luubieunghi/lbn/booklib/UI/ReadBook/new_UI/HighlightManagementActivity.java
@@ -1,6 +1,25 @@
 package luubieunghi.lbn.booklib.UI.ReadBook.new_UI;
 
-public class HighlightManagementActivity {
-// minh co path => luu vao bookId laf ok
-    
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.folioreader.ui.fragment.HighlightFragment;
+
+import luubieunghi.lbn.booklib.R;
+
+public class HighlightManagementActivity extends AppCompatActivity {
+    // minh co path => luu vao bookId laf ok
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.highlight_management_activity);
+        HighlightFragment highlightFragment = HighlightFragment.newInstance("urn:uuid:402eb356-5978-4c0e-9a23-1e63d8be1380", "");
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.test_layout, highlightFragment);
+        ft.commit();
+    }
 }
